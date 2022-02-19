0bd447f3c09944442e2656d0a0c069e6c744a7ea
jorlop89 <j.l.h1989@gmail.com>
2022-02-19 11:03:54 +0100

    Add project repository

---
+++ app/src/main/java/com/jorlop89/githubrepos/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jorlop89.githubrepos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jorlop89.githubrepos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
